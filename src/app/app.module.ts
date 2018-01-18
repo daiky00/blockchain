fc16288a88ce037804e1129b194f52695e626b9e
daiky00 <project.01@hotmal.com>
2018-01-17 23:35:55 -0600

    created a shared coins service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 // Router dependencies 
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+// shared services
+import { CoinsService } from './shared/services/coins.service';
+// containers
 import { HomeComponent } from './containers/home/home.component';
 import { AboutComponent } from './containers/about/about.component';
 import { PageNotFoundComponent } from './containers/page-not-found/page-not-found.component';
+
+// main app component
+import { AppComponent } from './app.component';
+
+// components
+import { HeaderComponent } from './components/header/header.component';
 import { PageTitleComponent } from './components/page-title/page-title.component';
 
 
@@ -25,13 +32,14 @@ import { PageTitleComponent } from './components/page-title/page-title.component
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
 
   ],
-  providers: [],
+  providers: [CoinsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
