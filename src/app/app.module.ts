fd034f9de6c3de7f1b3a8a54af09b92fa6ec8439
daiky00 <project.01@hotmal.com>
2018-01-17 21:22:58 -0600

    created page containers and added routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Router dependencies 
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './containers/home/home.component';
+import { AboutComponent } from './containers/about/about.component';
+import { PageNotFoundComponent } from './containers/page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
