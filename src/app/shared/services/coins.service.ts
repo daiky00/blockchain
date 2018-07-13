58a3e99f9182cb8b5d2a04bdb6e00a06cd60de37
Francisco Pena <project.01@hotmail.com>
2018-07-12 21:47:50 -0500

    update application to angular 6

--- src/app/shared/services/coins.service.ts
+++ src/app/shared/services/coins.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { environment } from '../../../environments/environment'
+import { environment } from '../../../environments/environment';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+  })
+
 export class CoinsService {
 
   API = environment.API;
-
 
   constructor(private http: Http) { }
 
@@ -20,14 +22,13 @@ export class CoinsService {
   }
 
   private handleError(error: any) {
-    const ERRORMESSAGE = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : "Service Error";
-    return Observable.throw(ERRORMESSAGE)
+    const ERRORMESSAGE = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Service Error';
+    return Observable.throw(ERRORMESSAGE);
   }
-
   getAllCoins() {
     return this.http.get(`${this.API}/front`)
-                    .map(this.extractData)
-                    .catch(this.handleError)
+               .map(this.extractData)
+               .catch(this.handleError);
   }
 
 }
